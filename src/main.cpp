89140a503374b3256f9c9ab356e6a0f65edc4da2
Rafale25 <rafale25.dev@gmail.com>
2026-03-14 02:01:55 +0100

    VMA

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-// #include <glm/glm.hpp>
+
+// #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+// #include <glm/gtc/matrix_transform.hpp>
+// #include <glm/gtc/quaternion.hpp>
+
+#define VMA_IMPLEMENTATION
+#include <vma/vk_mem_alloc.h>
 
 #include <iostream>
 #include <vector>
@@ -19,6 +26,7 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -26,7 +34,14 @@ int main(void)
         return -1;
     }
 
-    glfwMakeContextCurrent(window);
+    // glfwMakeContextCurrent(window);
+
+    if (!glfwVulkanSupported())
+    {
+        std::cerr << "GLFW doesn't support Vulkan?!!\n";
+		exit(-1);
+    }
+
 
 
     VkApplicationInfo appInfo {
@@ -123,6 +138,205 @@ int main(void)
 
     VkQueue queue{ VK_NULL_HANDLE };
     vkGetDeviceQueue(device, queueFamily, 0, &queue);
+
+
+    VmaVulkanFunctions vkFunctions{
+        .vkGetInstanceProcAddr = vkGetInstanceProcAddr,
+        .vkGetDeviceProcAddr = vkGetDeviceProcAddr,
+        .vkCreateImage = vkCreateImage
+    };
+    VmaAllocatorCreateInfo allocatorCI{
+        .flags = VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT,
+        .physicalDevice = devices[deviceIndex],
+        .device = device,
+        .pVulkanFunctions = &vkFunctions,
+        .instance = instance
+    };
+
+    VmaAllocator allocator{ VK_NULL_HANDLE };
+    chk(vmaCreateAllocator(&allocatorCI, &allocator));
+
+    VkSurfaceKHR surface{ VK_NULL_HANDLE };
+    chk(glfwCreateWindowSurface(instance, window, nullptr, &surface));
+
+    VkSurfaceCapabilitiesKHR surfaceCaps{};
+    chk(vkGetPhysicalDeviceSurfaceCapabilitiesKHR(devices[deviceIndex], surface, &surfaceCaps));
+
+    const VkFormat imageFormat{ VK_FORMAT_B8G8R8A8_SRGB };
+    VkSwapchainKHR swapchain{ VK_NULL_HANDLE };
+    VkSwapchainCreateInfoKHR swapchainCI{
+        .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+        .surface = surface,
+        .minImageCount = surfaceCaps.minImageCount,
+        .imageFormat = imageFormat,
+        .imageColorSpace = VK_COLORSPACE_SRGB_NONLINEAR_KHR,
+        .imageExtent{ .width = surfaceCaps.currentExtent.width, .height = surfaceCaps.currentExtent.height },
+        .imageArrayLayers = 1,
+        .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+        .preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR,
+        .compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+        .presentMode = VK_PRESENT_MODE_FIFO_KHR
+    };
+    chk(vkCreateSwapchainKHR(device, &swapchainCI, nullptr, &swapchain));
+
+    uint32_t imageCount{ 0 };
+    std::vector<VkImage> swapchainImages;
+    std::vector<VkImageView> swapchainImageViews;
+    chk(vkGetSwapchainImagesKHR(device, swapchain, &imageCount, nullptr));
+    swapchainImages.resize(imageCount);
+    chk(vkGetSwapchainImagesKHR(device, swapchain, &imageCount, swapchainImages.data()));
+    swapchainImageViews.resize(imageCount);
+
+    std::vector<VkFormat> depthFormatList{ VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT };
+    VkFormat depthFormat{ VK_FORMAT_UNDEFINED };
+    for (VkFormat& format : depthFormatList) {
+        VkFormatProperties2 formatProperties{ .sType = VK_STRUCTURE_TYPE_FORMAT_PROPERTIES_2 };
+        vkGetPhysicalDeviceFormatProperties2(devices[deviceIndex], format, &formatProperties);
+        if (formatProperties.formatProperties.optimalTilingFeatures & VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT) {
+            depthFormat = format;
+            break;
+        }
+    }
+
+    int framebufferWidth, framebufferHeight;
+    glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
+
+    VkImageCreateInfo depthImageCI{
+        .sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+        .imageType = VK_IMAGE_TYPE_2D,
+        .format = depthFormat,
+        // .extent{.width = window.getSize().x, .height = window.getSize().y, .depth = 1 },
+        .extent{.width = (uint32_t)framebufferWidth, .height = (uint32_t)framebufferHeight, .depth = 1 },
+        .mipLevels = 1,
+        .arrayLayers = 1,
+        .samples = VK_SAMPLE_COUNT_1_BIT,
+        .tiling = VK_IMAGE_TILING_OPTIMAL,
+        .usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+        .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+    };
+
+    VkImage depthImage;
+    VmaAllocation depthImageAllocation;
+    VmaAllocationCreateInfo allocCI{
+        .flags = VMA_ALLOCATION_CREATE_DEDICATED_MEMORY_BIT,
+        .usage = VMA_MEMORY_USAGE_AUTO
+    };
+    chk(vmaCreateImage(allocator, &depthImageCI, &allocCI, &depthImage, &depthImageAllocation, nullptr));
+
+    VkImageView depthImageView;
+    VkImageViewCreateInfo depthViewCI{
+        .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+        .image = depthImage,
+        .viewType = VK_IMAGE_VIEW_TYPE_2D,
+        .format = depthFormat,
+        .subresourceRange{ .aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT, .levelCount = 1, .layerCount = 1 }
+    };
+    chk(vkCreateImageView(device, &depthViewCI, nullptr, &depthImageView));
+
+
+    struct Vertex {
+        glm::vec3 pos;
+        glm::vec3 color;
+    };
+
+    const VkDeviceSize indexCount{3};
+    std::vector<Vertex> vertices{
+        {{0, 0, 0}, {1, 0, 0}},
+        {{1, 0, 0}, {0, 1, 0}},
+        {{0.5, 1, 0}, {0, 0, 1}},
+    };
+    std::vector<uint16_t> indices{
+        0, 1, 2,
+    };
+
+    VkDeviceSize vBufSize{ sizeof(Vertex) * vertices.size() };
+    VkDeviceSize iBufSize{ sizeof(uint16_t) * indices.size() };
+    VkBufferCreateInfo bufferCI{
+        .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+        .size = vBufSize + iBufSize,
+        .usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT
+    };
+
+
+    VkBuffer vBuffer{ VK_NULL_HANDLE };
+    VmaAllocation vBufferAllocation{ VK_NULL_HANDLE };
+    VmaAllocationCreateInfo vBufferAllocCI{
+        .flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_HOST_ACCESS_ALLOW_TRANSFER_INSTEAD_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT,
+        .usage = VMA_MEMORY_USAGE_AUTO
+    };
+    VmaAllocationInfo vBufferAllocInfo{};
+    chk(vmaCreateBuffer(allocator, &bufferCI, &vBufferAllocCI, &vBuffer, &vBufferAllocation, &vBufferAllocInfo));
+
+    memcpy(vBufferAllocInfo.pMappedData, vertices.data(), vBufSize);
+    memcpy(((char*)vBufferAllocInfo.pMappedData) + vBufSize, indices.data(), iBufSize);
+
+
+    constexpr uint32_t maxFramesInFlight{ 2 };
+    struct ShaderDataBuffer {
+        VmaAllocation allocation{ VK_NULL_HANDLE };
+        VmaAllocationInfo allocationInfo{};
+        VkBuffer buffer{ VK_NULL_HANDLE };
+        VkDeviceAddress deviceAddress{};
+    };
+    std::array<ShaderDataBuffer, maxFramesInFlight> shaderDataBuffers;
+    std::array<VkCommandBuffer, maxFramesInFlight> commandBuffers;
+
+    struct ShaderData {
+        glm::mat4 projection;
+        glm::mat4 view;
+        glm::mat4 model[3];
+        glm::vec4 lightPos{ 0.0f, -10.0f, 10.0f, 0.0f };
+        uint32_t selected{1};
+    } shaderData{};
+
+    for (uint32_t i = 0; i < maxFramesInFlight; ++i) {
+        VkBufferCreateInfo uBufferCI{
+            .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+            .size = sizeof(ShaderData),
+            .usage = VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT
+        };
+        VmaAllocationCreateInfo uBufferAllocCI{
+            .flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_HOST_ACCESS_ALLOW_TRANSFER_INSTEAD_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT,
+            .usage = VMA_MEMORY_USAGE_AUTO
+        };
+        chk(vmaCreateBuffer(allocator, &uBufferCI, &uBufferAllocCI, &shaderDataBuffers[i].buffer, &shaderDataBuffers[i].allocation, &shaderDataBuffers[i].allocationInfo));
+
+        VkBufferDeviceAddressInfo uBufferBdaInfo{
+            .sType = VK_STRUCTURE_TYPE_BUFFER_DEVICE_ADDRESS_INFO,
+            .buffer = shaderDataBuffers[i].buffer
+        };
+        shaderDataBuffers[i].deviceAddress = vkGetBufferDeviceAddress(device, &uBufferBdaInfo);
+    }
+
+    std::array<VkFence, maxFramesInFlight> fences;
+    std::array<VkSemaphore, maxFramesInFlight> presentSemaphores;
+    std::vector<VkSemaphore> renderSemaphores;
+    VkSemaphoreCreateInfo semaphoreCI{
+        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO
+    };
+    VkFenceCreateInfo fenceCI{
+        .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+        .flags = VK_FENCE_CREATE_SIGNALED_BIT
+    };
+    for (uint32_t i = 0; i < maxFramesInFlight; i++) {
+        chk(vkCreateFence(device, &fenceCI, nullptr, &fences[i]));
+        chk(vkCreateSemaphore(device, &semaphoreCI, nullptr, &presentSemaphores[i]));
+    }
+    renderSemaphores.resize(swapchainImages.size());
+    for (auto& semaphore : renderSemaphores) {
+        chk(vkCreateSemaphore(device, &semaphoreCI, nullptr, &semaphore));
+    }
+
+    VkCommandPool commandPool{ VK_NULL_HANDLE };
+    VkCommandPoolCreateInfo commandPoolCI{
+        .sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+        .flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
+        .queueFamilyIndex = queueFamily
+    };
+    chk(vkCreateCommandPool(device, &commandPoolCI, nullptr, &commandPool));
+
+
+
 
 
     while (!glfwWindowShouldClose(window))
